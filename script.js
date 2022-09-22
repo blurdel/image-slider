de5c251094af37a5bdb1654ca5a2271de58034ca
blurdel <5648737+blurdel@users.noreply.github.com>
2022-09-21 20:18:48 -0400

    This version does not wrap around, it reverts back

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 "use strict";
-// Select all slides
+
 const slides = document.querySelectorAll(".slide");
+const prevSlide = document.querySelector(".btn-prev");
+const nextSlide = document.querySelector(".btn-next");
+
 
 // loop through slides and set each slides translateX
 slides.forEach((slide, indx) => {
     slide.style.transform = `translateX(${indx * 100}%)`;
 });
 
-// select next slide button
-const nextSlide = document.querySelector(".btn-next");
 
-// current slide counter
 let curSlide = 0;
-// maximum number of slides
 let maxSlide = slides.length - 1;
 
 // add event listener and navigation functionality
@@ -24,16 +23,12 @@ nextSlide.addEventListener("click", function () {
         curSlide++;
     }
 
-    //   move slide by -100%
+    // move slide by -100%
     slides.forEach((slide, indx) => {
         slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
     });
 });
 
-// select next slide button
-const prevSlide = document.querySelector(".btn-prev");
-
-// add event listener and navigation functionality
 prevSlide.addEventListener("click", function () {
     // check if current slide is the first and reset current slide to last
     if (curSlide === 0) {
@@ -42,7 +37,7 @@ prevSlide.addEventListener("click", function () {
         curSlide--;
     }
 
-    //   move slide by 100%
+    // move slide by 100%
     slides.forEach((slide, indx) => {
         slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
     });
